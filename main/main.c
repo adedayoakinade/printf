c3c4008c87827f0cdc280d40cf51ea3a20aa7305
vierukundo <vierukundo@gmail.com>
2023-03-28 10:16:09 -0700

    removed main file

--- main/main.c
+++ main/main.c
@@ -1,10 +1,17 @@
 #include "main.h"
+/**
+ * main - main
+ * Return: 0
+ */
 int main(void)
 {
 	int n = -45, m = 45;
+
 	char c = 'F';
+
 	char *s = "Is it working";
-	_printf("This is test for octal %o string %s hex %X,%x and binary %b character %c Bufer\n", n, s, n, n, n, c);
+
+	_printf("This is test for octal %o string %s hex %X,%x anid binary %b character %c Bufer\n", n, s, n, n, n, c);
 	_printf("This is test for octal %o string %s hex %X,%x and binary %b character %c Bufer\n", m, s, m, m, m, c);
 	printf("%d, %X, %o, %o\n", n, n, n, n);
 	printf("%d, %X, %o, %o\n", m, m, m, m);
